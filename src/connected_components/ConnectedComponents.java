89b51ddbd856b14965da00ce9b0b34a2b4b91486
pmatushkin <pmatushkin@gmail.com>
2016-08-16 19:59:02 -0400

    feat: Pass Problem 2: Adding Exits to a Maze

--- src/connected_components/ConnectedComponents.java
+++ src/connected_components/ConnectedComponents.java
@@ -5,7 +5,46 @@ public class ConnectedComponents {
     private static int numberOfComponents(ArrayList<Integer>[] adj) {
         int result = 0;
         //write your code here
-        return result;
+        return dfs(adj);
+    }
+
+    private static int dfs(ArrayList<Integer>[] adj) {
+        // initialize component counter
+        int cc = 0;
+
+        // initialize array of 'visited' flags
+        int[] v = new int[adj.length];
+
+        // for each vertex in a graph...
+        for (int i = 0; i < adj.length; i++) {
+            // ... explore if it wasn't explored
+            if (v[i] != 1) {
+                explore(adj, v, i);
+
+                // increment component counter,
+                // because we just finished exploring
+                // another connected component
+                cc++;
+            }
+        }
+
+        return cc;
+    }
+
+    private static void explore(ArrayList<Integer>[] adj, int[] v, int x) {
+        // mark the current vertex as visited
+        v[x] = 1;
+
+        // get the list of edges connected to the current vertex
+        ArrayList<Integer> edges = adj[x];
+
+        // for every edge connected to the current vertex
+        for (int e : edges) {
+            // begin exploring starting from the next vertex
+            if (v[e] != 1) {
+                explore(adj, v, e);
+            }
+        }
     }
 
     public static void main(String[] args) {
